991a23854d5e8840f29454dbd627c6d15fb806a9
mmitura <mitura.mikolaj@gmail.com>
2021-07-31 12:40:59 +0200

    resolving BeanMethodMetaModel for example return type, and parameters with annotations.

--- crud-wizard-core/src/main/java/pl/jalokim/crudwizard/core/metamodels/EndpointMetaModel.java
+++ crud-wizard-core/src/main/java/pl/jalokim/crudwizard/core/metamodels/EndpointMetaModel.java
@@ -31,6 +31,7 @@ public class EndpointMetaModel extends AdditionalPropertyMetaModelDto {
 
     // TODO validation when added new service metamodel
     // verify that this bean, class, method exists
+    // verify that can method arguments will be resolved correctly
     // verify that newly added serviceMetaModel does not exists already, then use existing id
     ServiceMetaModel serviceMetaModel;
 
